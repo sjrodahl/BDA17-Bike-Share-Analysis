13264f10c4ab29425478d78d0405087b3aac3983
sjrodahl <son.rodahl@gmail.com>
2017-05-09 11:03:43 -0700

    Added plotting script with gender plot

--- plots.R
+++ plots.R
@@ -7,11 +7,12 @@
 
 library(ggplot2)
 
-trip.df <- read.csv("trip.csv")
+trip.df <- read.csv("D:/Sondre/Dokumenter/UCSD/IRGN452 BigDataAnalytics/Project/BDA17-Bike-Share-Analysis/data/trip.csv")
 
-ggplot(data=subset(trip.df, gender!='')) + geom_histogram(aes(x=gender) , stat="count")
-
-ggplot(data=female.df) + geom_point(aes(x=tripduration, y = starttime))
+#Plot of number of trips split by gender. Observations without he gender variable is omitted. Category "other" is included
+#gender-count.png
+trip.gender <- subset(trip.df, gender!='')
+ggplot(data=trip.gender) + geom_histogram(aes(x=gender),  fill = "#FF9911", colour = "black", stat="count")
 
 males.df <- subset(trip.df, gender=="Male")
 females.df <- subset(trip.df, gender =="Female")
