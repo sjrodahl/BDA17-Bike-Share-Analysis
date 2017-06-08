10406ce78c731b0f07bf5e6450808257fad278e7
sjrodahl <son.rodahl@gmail.com>
2017-06-07 18:47:53 -0700

    Calculated average

--- av_trips_per_bike_per_day.R
+++ av_trips_per_bike_per_day.R
@@ -13,6 +13,9 @@ trips$onlyDate<-as.Date(trips$startDate,tz = "PST8PDT")
 #trips$onlyDate<-factor(trips$onlyDate)
 trips$bikeid<-factor(trips$bikeid)
 
+trips$season<-time2season(trips$onlyDate, out.fmt = "seasons")
+trips$season<-as.factor(trips$season)
+
 av_trips_per_bike_per_day <- (nrow(trips)/nlevels(trips$bikeid))/nlevels(trips$onlyDate)
 
 # = 1.1266674 trips/day/bike
@@ -25,11 +28,18 @@ mean_grpd <- dplyr::summarise(date_grpd, mean = mean(departures))
 
 grpd_bike <- group_by(trips, onlyDate, add = FALSE)
 num_unique_bikes <- dplyr::summarise(grpd_bike, unq_bikes = n_distinct(bikeid))
+num_unq_bikes_percent <- dplyr::summarise(grpd_bike, unq_bikes = 100*n_distinct(bikeid)/nlevels(bikeid))
+yrng <-range(num_unq_bikes_percent$unq_bikes)
+#season_shade <- geom_rect(aes(NULL, NULL, xmin=onlyDate, xmax = onlyDate, fill = season), ymin=yrng[1], ymax=yrng[2], data=trips)
 
-ggplot(data=num_unique_bikes, aes(x=onlyDate, y=unq_bikes, group=1))+
+unq_plot<-ggplot(data=num_unq_bikes_percent, aes(x=onlyDate, y=unq_bikes, group=1))+
   geom_line() +
   scale_x_date("Date") +
-  scale_y_continuous("Number of bikes used")+
-  ggtitle("Number of unique bikes used per day")
+  scale_y_continuous("Percentage of total bike fleet")+
+  ggtitle("Percentage of total bike fleet used per day")
                  
+unq_plot_shade <- unq_plot+season_shade
+
+grp_bikeid <- group_by(trips, bikeid ) %>% dplyr::summarise(departures=n())
+
        
